cb6c372093abbde72b7e623413a48562b58b9dc4
Tatiana-goit <tanchou74@gmail.com>
2022-03-18 15:22:04 +0200

    all work

---
+++ src/Loader.js
@@ -0,0 +1,8 @@
+function Loader() {
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', margin: '.5rem' }}>
+      <div className="lds-dual-ring" />
+    </div>
+  )
+}
+export default Loader
